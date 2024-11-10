254f7d878e5ab3494b85601dca2c69b8380febfa
Jakob Olive <jakolive01@gmail.com>
2024-11-09 19:07:45 -0500

    Styling Added
    The majority of styling using CSS was added as well as the header and footer. Now to add Strapi data and display it onto the website.

---
+++ Project/my-next-app/src/components/footer/footer.js
@@ -0,0 +1,15 @@
+
+export default function Footer(){
+    return (
+        <>
+        <div class="footer-content">
+        <p>&copy; 2024 Your Company Name. All Rights Reserved.</p>
+        <ul class="footer-links">
+        <li><a href="#privacy-policy">Privacy Policy</a></li>
+        <li><a href="#terms-of-service">Terms of Service</a></li>
+        <li><a href="#contact-us">Contact Us</a></li>
+    </ul>
+  </div>
+        </>
+    )
+}
--- Project/my-next-app/src/pages/_document.js
+++ Project/my-next-app/src/pages/_document.js
@@ -1,13 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Footer from '../components/footer/footer';
+import Header from "@/components/header/header";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <Header />
         <Main />
         <NextScript />
+        <Footer />
       </body>
     </Html>
   );
 }
+
